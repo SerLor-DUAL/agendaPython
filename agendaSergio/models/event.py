1d24400b31fabb89cfea4ff20eeb3a418e9384bb
Santiago Lorenzano <enderwigim@gmail.com>
2025-05-28 17:11:29 +0200

    formatted date created.

--- agendaSergio/models/event.py
+++ agendaSergio/models/event.py
@@ -1,15 +1,22 @@
+import datetime
+
 
 # models/event.py
 class Event:
-    def __init__(event, id, title, description, startTime, endTime, user):
+    def __init__(event, id, title, description, startDate, startTime, endDate, endTime, user):
         event.id = id
         event.title = title
         event.description = description
-        event.startTime = startTime
-        event.endTime = endTime
+        event.startTime = event.formatDate(startDate, startTime)
+        event.endTime = event.formatDate(endDate, endTime)
         event.eventUser = user
     
-    
+    def formatDate(date, hour):
+        """Gathers information and returns a datetime"""
+        newDate = date.split("/")
+        newHour = hour.split(":")
+        return datetime.datetime(newDate[0], newDate[1], newDate[2], newHour[0], newHour[1])
+
     def __repr__(event):
         """Return a JSON representation of the Event object."""
         eventJSON = {
