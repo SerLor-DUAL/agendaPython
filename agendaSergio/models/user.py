6779bf03b1e2b5a96131b3dd44b13a96e4f02bee
Sergio Diez Fernández <145585172+SerDieFer@users.noreply.github.com>
2025-05-28 17:24:48 +0200

    Renamed

--- agendaSergio/models/user.py
+++ agendaSergio/models/user.py
@@ -17,22 +17,22 @@ class User:
         return selfJSON
     
     # Add methods to manage events for the user
-    def add_event(user, title, description, start_time, end_time):
+    def addEvent(user, title, description, start_time, end_time):
         """Add an event to the user's list of events."""
         eventToAdd = Event(title, description, start_time, end_time, eventUser = user) # Create a new Event object for this user
         user.events.append(eventToAdd)  # Append the new event to the user's events list
     
     # List all events associated with the user
-    def list_events(user):
+    def listEvents(user):
         """List all events associated with the user."""
         return [event.__repr__() for event in user.events]
     
     # Remove an event from the user's list of events
-    def remove_event(user, eventId):
+    def removeEvent(user, eventId):
         """Remove an event from the user's list of events by ID."""
         user.events.pop(eventId)  # Remove the event with the specified ID from the user's events list
     
     # Edit an existing event's details   
-    def edit_event(user, eventId, title=None, description=None, start_time=None, end_time=None):
+    def editEvent(user, eventId, title=None, description=None, start_time=None, end_time=None):
         """Edit an existing event's details."""
         user.events[eventId].editEvent(title, description, start_time, end_time)
