868e0714fe5519c6fd354ed444bac100249ab044
Santiago Lorenzano <enderwigim@gmail.com>
2025-05-28 17:58:28 +0200

    userList created

---
+++ agendaSergio/models/userList.py
@@ -0,0 +1,7 @@
+from models import User
+class UserList:
+    users = []
+    def addUser(self, user):
+        self.users.append(user)
+    def deleteUserByID(self, userID):
+        self.users.pop(userID)
