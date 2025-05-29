141c133609717f13a6ee0fc9280eab22b2ba3a49
Santiago Lorenzano <enderwigim@gmail.com>
2025-05-29 17:50:54 +0200

    menu edit

--- models/menu/menuHandler.py
+++ models/menu/menuHandler.py
@@ -6,20 +6,26 @@ class MenuHandler:
     def __init__(self):
         self.isRunning = True
         self.active_menu = MenuDisplay()
+        self.currentUser = None
     def showMenu(self):
         userInput = self.active_menu.launchMenu()
         if type(self.active_menu) is MenuDisplay:
             if userInput == 1:
                 self.active_menu = CreateUser()
             if userInput == 2:
-                self.active_menu.showUsers(userList)
+                self.active_menu.showUsers(userList.users)
 
         elif isinstance(self.active_menu, CreateUser):
+            user = self.active_menu.createUser()
+            result = userList.addUser(user)
+            if result:
+                print(f"Usuario creado correctamente. Bienvenido {user.name}")
             self.active_menu = UserMenu()
 
         elif isinstance(self.active_menu, UserMenu):
             if userInput == 1:
                 self.active_menu = CreateEventMenu()
+                
             elif userInput == 2:
                 self.active_menu = ShowEvents()
 
--- models/userList.py
+++ models/userList.py
@@ -1,7 +1,19 @@
+from .user import User
+
 class UserList:
     users = []
     def addUser(self, user):
-        self.users.append(user)
+        # No crear el usuario si el nombre ya existe
+        name = user["name"]
+        for existing_user in self.users:
+            if existing_user.name == name:
+                print(f"Ya existe un usuario con el nombre: {name}")
+                return False
+
+        newUserID = len(self.users) + 1
+        newUser = User(newUserID, name)
+        self.users.append(newUser)
+        
     def deleteUserByID(self, userID):
         self.users.pop(userID)
     def getUsers(self):
